c2bd860f84350aba7c2997967118fe10c23e11af
GastonTrombotti <112663133+GastonTrombotti@users.noreply.github.com>
2025-04-19 19:51:25 -0300

    fixed player controller issues on ovewrold map

--- Crazy Mechanics/Assets/Scripts/WorldSelect/WorldObject.cs
+++ Crazy Mechanics/Assets/Scripts/WorldSelect/WorldObject.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
@@ -5,16 +6,24 @@ public class WorldObject : MonoBehaviour, IInteractable
 {
     [SerializeField] string worldToGo = "Test Demo";
     [SerializeField] private UnityEvent _onInteract;
+    [SerializeField] Animator transitionAnimation;
     UnityEvent IInteractable.OnInteract { 
         get => _onInteract;
         set => _onInteract  = value;   
     }
 
+    IEnumerator LoadLevel(){
+        transitionAnimation.SetTrigger("End");
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(worldToGo);
+        transitionAnimation.SetTrigger("Start");
+    }
     public void Interact () => _onInteract.Invoke();
 
     public void LoadWorld () {
+       
         Debug.Log("world object interaction");
-        SceneManager.LoadScene(worldToGo);
+        StartCoroutine(LoadLevel());
     }
 
 }
