fa0dfb096df12592f6c2a9974e84509fc8633720
GastonTrombotti <112663133+GastonTrombotti@users.noreply.github.com>
2025-05-08 16:21:22 -0300

    limpieza de algo de codigo en world select

--- Crazy Mechanics/Assets/Scripts/WorldSelect/PanelTrigger.cs
+++ Crazy Mechanics/Assets/Scripts/WorldSelect/PanelTrigger.cs
@@ -6,21 +6,18 @@ public class WorldSelectManager : MonoBehaviour
     [SerializeField] private GameObject StagePanel;
     [SerializeField] private CanvasGroup Canvas;
     private bool FadeIn = false;
-    private bool FadeOut = false;
     private void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Player")){
-            //StagePanel.SetActive(true);
             Debug.Log("fading in/n");
-            FadeIn=true;
+            FadeIn = true;
         }
     }
 
     void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player")){
-            //StagePanel.SetActive(false);
             Debug.Log("fading out/n");
-            FadeOut=true;
+            FadeIn = false;
         }
     }
 
@@ -30,17 +27,13 @@ public class WorldSelectManager : MonoBehaviour
             if(Canvas.alpha < 1){
                 Canvas.alpha+=Time.deltaTime;
             }
-            else{
-                FadeIn=false;
-            }
+            
         }
-        if(FadeOut && !FadeIn){ //Added "!FadeIn" to correct bug where one leave the area before the canvas completes the fade in animation causing both fade in and fed out to trigger simultaneusly
+        
+        else{
             if(Canvas.alpha>0){
                 Canvas.alpha-=Time.deltaTime;
             }  
-            else{
-                FadeOut=false;
-            }
         }
     }
 }
--- Crazy Mechanics/Assets/Scripts/WorldSelect/PlayerController.cs
+++ Crazy Mechanics/Assets/Scripts/WorldSelect/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
     private Vector2 input;
     private CharacterController  playerController;
     private Vector3 direction;
+
+    private Vector3 castDirection;
     [SerializeField] private float speed;
     [SerializeField] private float smoothTurnigTime = 0.5f;
     [SerializeField] LayerMask objectLayerMask;
@@ -29,7 +31,7 @@ public class PlayerController : MonoBehaviour
             return;
         }
 
-        // Character angule rotation calculation
+        // calculo de angulo de rotación de personaje
         var facing = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
         var turnAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, facing, ref currentVelocityTurn, smoothTurnigTime);
         transform.rotation = Quaternion.Euler(0, turnAngle, 0);
@@ -55,6 +57,9 @@ public class PlayerController : MonoBehaviour
     {
         input = context.ReadValue<Vector2>();
         direction = new Vector3(input.x, 0.0f, input.y);
+        if(direction !=Vector3.zero){
+            castDirection = direction; // Actualiza donde mira el jugador
+        }
     }
 
     public void Pause(InputAction.CallbackContext context)
@@ -63,29 +68,30 @@ public class PlayerController : MonoBehaviour
     }
 
     public void Interact (InputAction.CallbackContext context) {
-        Vector3 castDirection = new Vector3(input.x, 0f, input.y);
+        //Vector3 castDirection = new Vector3(input.x, 0f, input.y);
         //Interaction capsule sizes
-        float capsuleDistance = 3f;
+        float capsuleDistance = 6f;
         float capsuleRadius = 0.5f;
         float capsuleHeight = 1.5f;
 
         Vector3 capsuleStart = transform.position;
         Vector3 capsuleEnd = transform.position + Vector3.up*capsuleHeight;
 
-        if(castDirection != Vector3.zero){
-            direction = castDirection; //Make facing and interaction direction the same
+        if(castDirection == Vector3.zero){
+            castDirection = direction; //Make facing and interaction direction the same
         }
-        if(Physics.CapsuleCast(capsuleStart,capsuleEnd, capsuleRadius, direction, out RaycastHit raycastHit, capsuleDistance, objectLayerMask)){//Raycast hitting a interactable object
+        /*if(Physics.CapsuleCast(capsuleStart,capsuleEnd, capsuleRadius, direction, out RaycastHit raycastHit, capsuleDistance, objectLayerMask)){//Raycast hitting a interactable object
             Debug.Log("Raycast hit object");
             if(raycastHit.transform.TryGetComponent(out InteractableObject interactable)){
                 Debug.Log("calling interact from player controller");
                 interactable.Interact();                
             }
-        }
-        if(Physics.CapsuleCast(capsuleStart,capsuleEnd, capsuleRadius, direction, out raycastHit, capsuleDistance, levelLayerMask)){//Raycast hiting a level select object
-            Debug.Log("Raycast hit world" + raycastHit.transform.name);
+        }*/
+        Debug.Log("where " + castDirection);
+        if(Physics.CapsuleCast(capsuleStart,capsuleEnd, capsuleRadius, castDirection, out RaycastHit raycastHit, capsuleDistance, levelLayerMask)){//Raycast hiting a level select object
+            //Debug.Log("Raycast hit world" + raycastHit.transform.name);
             if(raycastHit.transform.TryGetComponent(out WorldObject worldObject)){
-                Debug.Log("calling world from player controller");
+                //Debug.Log("calling world from player controller");
                 worldObject.Interact();                
             }    
         }
