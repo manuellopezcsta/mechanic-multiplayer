8f2c0d42e4d2260648c5b10bd113702cac94f7cb
GastonTrombotti <112663133+GastonTrombotti@users.noreply.github.com>
2025-05-23 18:05:33 -0300

    Overworld Mannager trabaja coon player prefs

--- Crazy Mechanics/Assets/Scripts/WorldSelect/OverworldMannager.cs
+++ Crazy Mechanics/Assets/Scripts/WorldSelect/OverworldMannager.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 
 public class OverworldMannager : MonoBehaviour
 {
+    public static OverworldMannager instance { get; private set; }
     private int lastLevelLoaded;
+    const string KEY = "LastLevelLoaded";
     [SerializeField] Transform[] levelSpawns;
     [SerializeField] Transform playerPosition;
 
     void Start()
     {
         playerPosition.transform.position = levelSpawns[lastLevelLoaded].transform.position;
+        if (PlayerPrefs.HasKey(KEY))//Si ya esta cargada un ultimo nivel accedido de la ultima sesión lo carga
+        {
+            lastLevelLoaded = PlayerPrefs.GetInt(KEY);
+        }
     }
 
     public void changeLasteLevelLoade(int level)
     {
-        lastLevelLoaded = level;
+        PlayerPrefs.SetInt(KEY, level);
     }
 
     public int getLastLevelLoaded()
