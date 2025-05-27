968c4ab422be583dc5a9f0815914b95b065e3280
IgnChamo <ichamorro2208@gmail.com>
2025-05-27 18:27:54 -0300

    Level 6

--- Crazy Mechanics/Assets/Scripts/Loading/Loader.cs
+++ Crazy Mechanics/Assets/Scripts/Loading/Loader.cs
@@ -20,7 +20,9 @@ public static class Loader
         Loading,
         Level1,
         Level2,
-        Level3
+        Level3,
+        Level4,
+        Level5
     }
 
     private static Scene targetScene;
