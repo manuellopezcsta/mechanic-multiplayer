425536c41783bb00f2014e6391b2d99472b102b8
IgnChamo <ichamorro2208@gmail.com>
2025-06-24 17:00:28 -0300

    TutorialManager Change

--- Crazy Mechanics/Assets/Scripts/Elevator/ElevatorCounter.cs
+++ Crazy Mechanics/Assets/Scripts/Elevator/ElevatorCounter.cs
@@ -7,9 +7,31 @@ public class ElevatorCounter : BaseCounter
 {
     // Codigo de los botones del elevador.
     [SerializeField] ElevatorController elevatorController;
+    [SerializeField] ParticleSystem[] effects;
+    void Start()
+    {
+        if (effects.Length != 0)
+        {
+            foreach (ParticleSystem efect in effects)
+            {
+                efect.Stop();
+            }
+        }
+    }
     public override void Interact(Player player)
     {
         elevatorController.ChangeFloorElevator();
         Debug.Log("Piso actual: " + elevatorController.floorNumberElevator);
+        ActiveEfects();
+    }
+    private void ActiveEfects()
+    {
+        if (effects.Length != 0)
+        {
+            foreach (ParticleSystem efect in effects)
+            {
+                efect.Play();
+            }
+        }
     }
 }
